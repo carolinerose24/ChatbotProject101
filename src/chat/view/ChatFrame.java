6227cbad17f9eaa8521ba66b481449ded1121677
Caroline Jarman <carolinerosejarman@gmail.com>
2018-12-03 09:38:29 -0700

    Added code to Chat frame and panel

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -1,6 +1,30 @@
 package chat.view;
+import javax.swing.JFrame;
 
-public class ChatFrame
+import chat.controller.ChatController;
+
+
+public class ChatFrame extends JFrame
 {
-
+	private ChatController appController;
+	private ChatPanel appPanel;
+	
+	public ChatFrame(ChatController appController)
+	{
+		super();
+		
+		this.appController = appController;
+		this.appPanel = new ChatPanel(appController);
+		
+		setUpFrame();
+	}
+	
+	private void setUpFrame()
+	{
+		this.setContentPane(appPanel);
+		this.setSize(500, 500);
+		this.setTitle("Chatbot");
+		this.setResizable(false);
+		this.setVisible(true);
+	}
 }
--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -1,6 +1,56 @@
 package chat.view;
 
-public class ChatPanel
-{
+import javax.swing.*;
 
+import chat.controller.ChatController;
+
+import java.awt.Color;
+
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+
+
+public class ChatPanel extends JPanel
+{
+	private SpringLayout appLayout;
+	private ChatController appController;
+	private JButton chatButton;
+	private JTextField inputField;
+	private JTextArea chatArea;
+	private JScrollPane chatPane;
+	
+	
+	public ChatPanel(ChatController appController)
+	{
+		super();
+		this.appController = appController;
+		appLayout = new SpringLayout();
+		
+		setUpPanel();
+		setUpLayout();
+		setUpListeners();
+		setUpScrollPane();
+		
+	}
+	
+	private void setUpPanel()
+	{
+		
+	}
+	
+	private void setUpLayout()
+	{
+		
+	}
+	
+	private void setUpListeners()
+	{
+		
+	}
+	
+	private void setUpScrollPane()
+	{
+		
+	}
 }
