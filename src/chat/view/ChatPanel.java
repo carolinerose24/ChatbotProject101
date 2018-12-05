c77463aab4986b6b3f16cd60c6e5ddb0217abbf8
Caroline Jarman <carolinerosejarman@gmail.com>
2018-12-05 07:57:58 -0700

    Added data members to chat panel

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -15,10 +15,14 @@ public class ChatPanel extends JPanel
 {
 	private SpringLayout appLayout;
 	private ChatController appController;
-	private JButton chatButton;
-	private JTextField inputField;
+	private JTextField chatField;
 	private JTextArea chatArea;
 	private JScrollPane chatPane;
+	
+	private JButton ChatButton;
+	private JButton CheckerButton;
+	private JButton LoadButton;
+	private JButton SaveButton;
 	
 	
 	public ChatPanel(ChatController appController)
@@ -26,6 +30,16 @@ public class ChatPanel extends JPanel
 		super();
 		this.appController = appController;
 		appLayout = new SpringLayout();
+		
+		ChatButton = new JButton ("Chat");
+		CheckerButton = new JButton ("Check Text");
+		LoadButton = new JButton ("Load");
+		SaveButton = new JButton ("Save");
+		
+		chatField = new JTextField("Talk to the bot here", 50);
+		chatArea = new JTextArea("Chat Area", 20 ,50);
+		
+		chatPane = new JScrollPane();
 		
 		setUpPanel();
 		setUpLayout();
@@ -36,7 +50,18 @@ public class ChatPanel extends JPanel
 	
 	private void setUpPanel()
 	{
+	
+		this.add(chatField);
+		this.add(chatArea);
+		this.add(chatPane);
 		
+
+		this.add(ChatButton);
+		this.add(CheckerButton);
+		this.add(LoadButton);
+		this.add(SaveButton);
+		
+		this.setLayout(appLayout);
 	}
 	
 	private void setUpLayout()
@@ -46,11 +71,53 @@ public class ChatPanel extends JPanel
 	
 	private void setUpListeners()
 	{
+		ChatButton.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent mouseClick)
+			{
+				
+			}
+		});
+		
+		CheckerButton.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent mouseClick)
+			{
+				
+			}
+		});
+		
+		LoadButton.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent mouseClick)
+			{
+				
+			}
+		});
+		
+		SaveButton.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent mouseClick)
+			{
+				
+			}
+		});
 		
 	}
 	
 	private void setUpScrollPane()
 	{
+		chatArea.setEditable(false);
+		chatArea.setLineWrap(true);
+		chatArea.setWrapStyleWord(true);
+		
+		chatPane.setViewportView(chatArea);
+		chatPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		chatPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);		
 		
 	}
+	
+	
+	
+	
 }
